39fc1e6a8664df1eb4d8aefd7ec9ea66767360ce
yuushigure27 <yuushigure1227@gmail.com>
2024-06-22 16:01:19 +0000

    [finish]chapter10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
   devise_for :users
-  get 'homes/top'
-root to: "homes#top"
+  resources :post_images, only: [:new, :index, :show]
+  
+  get "/homes/about" => "homes#about", as: "about"
+  
 end
